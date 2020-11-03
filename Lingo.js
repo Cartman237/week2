ade6eb103e30076cde95f2b7f5c3d9018a2c9745
Sebastien Ooteman <sebas-gamer@hotmail.com>
2020-11-03 12:34:41 +0100

    week2

--- Lingo.js
+++ Lingo.js
@@ -8,16 +8,15 @@ function randomiser(bieb){
 var resultaat = randomiser(lingobieb);
 console.log(resultaat);
 */
-
 function randomiser(bieb){
     let random = Math.floor(Math.random() * bieb.length);
     let lingowoord = lingobieb[random];
     return lingowoord;
 }
-var resultaat = randomiser(lingobieb); // EERSTE LETTER VAN RANDOMISER
-for (var u = 0; u < resultaat.length ; u++){
+var resultaat = randomiser(lingobieb);
+for (let u = 0; u < resultaat.length ; u++){
     console.log(resultaat[u]);
-    document.getElementById("1A").value = resultaat[0];
+    document.getElementById("r0v0").value = resultaat[0]; // EERSTE LETTER VAN RANDOMISER
 }
 /*
 var rij1 = ("1A","1B","1C","1D","1E");
@@ -39,39 +38,69 @@ function invoer(){
     }
 }
 */
+var r = 0;
+var v = 0;
 function invoer(){
+    document.getElementById("r0v0").value = resultaat[0];
     let woord = document.getElementById("invoer").value;
-    console.log(woord);
-/*    if( woord.length == 5 ){
+/*    console.log(woord);
+    if( woord.length == 5 ){
         alert("CORRECT: Wel 5 letters!");
     }
     else{
         alert("INCORRECT: Niet 5 letters")
-    } */
-    document.getElementById("invoer").value; // EERSTE RIJ ANTWOORD
-    document.getElementById("1A").value = woord [0];
-    document.getElementById("1B").value = woord [1];
-    document.getElementById("1C").value = woord [2];
-    document.getElementById("1D").value = woord [3];
-    document.getElementById("1E").value = woord [4];
-    if( woord[3] == resultaat[3] ){
-        alert("Correct geraden!");
-        console.log(woord[4]);
-        console.log(resultaat[4]);
+    }
+    document.getElementById("invoer").value;
+    document.getElementById("r0v0").value = woord [0];
+    document.getElementById("r0v1").value = woord [1];
+    document.getElementById("r0v2").value = woord [2];
+    document.getElementById("r0v3").value = woord [3];
+    document.getElementById("r0v4").value = woord [4]; */
+    for( let w = 0 ; w < woord.length ; w++ ){
+        console.log(woord[w]);
+        document.getElementById("r"+r+"v"+w).value = woord[w];
+    }
+    if( woord != resultaat){
+        console.log("FOUT!");
+        alert("FOUT!");
+        document.getElementById("r"+r+"v0").value = resultaat[0];
     }
     else{
-        alert("Incorrect geraden!")
+        console.log("GOED!");
+        alert("GOED!");
+        document.getElementById("invoer").disabled = true;
     }
+    r++;
     document.getElementById("invoer").value = '';
 }
 
 function nieuw(){
     document.getElementById("invoer").value = '';
-    document.getElementById("1A").value = '';
-    document.getElementById("1B").value = '';
-    document.getElementById("1C").value = '';
-    document.getElementById("1D").value = '';
-    document.getElementById("1E").value = '';
+    document.getElementById("r0v0").value = '';
+    document.getElementById("r0v1").value = '';
+    document.getElementById("r0v2").value = '';
+    document.getElementById("r0v3").value = '';
+    document.getElementById("r0v4").value = '';
+    document.getElementById("r1v0").value = '';
+    document.getElementById("r1v1").value = '';
+    document.getElementById("r1v2").value = '';
+    document.getElementById("r1v3").value = '';
+    document.getElementById("r1v4").value = '';
+    document.getElementById("r2v0").value = '';
+    document.getElementById("r2v1").value = '';
+    document.getElementById("r2v2").value = '';
+    document.getElementById("r2v3").value = '';
+    document.getElementById("r2v4").value = '';
+    document.getElementById("r3v0").value = '';
+    document.getElementById("r3v1").value = '';
+    document.getElementById("r3v2").value = '';
+    document.getElementById("r3v3").value = '';
+    document.getElementById("r3v4").value = '';
+    document.getElementById("r4v0").value = '';
+    document.getElementById("r4v1").value = '';
+    document.getElementById("r4v2").value = '';
+    document.getElementById("r4v3").value = '';
+    document.getElementById("r4v4").value = '';
     location.reload();
 }
 
